81ffdcb0ccfa683b08501280c2aba948551b4735
rehanmujawarofficial <rehanmujawarofficial007@gmail.com>
2025-05-13 14:30:58 +0530

    added my app sorce code

---
+++ mywebapp/app/src/main/java/com/example/mywebapp/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.mywebapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://myai007.netlify.app/";
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 🔵 Make status bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        if (!CheckNetwork.isInternetAvailable(this)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("No internet connection available")
+                    .setMessage("Please Check your Mobile data or Wifi network.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        } else {
+            webview = findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+            webview.loadUrl(websiteURL);
+            webview.setWebViewClient(new WebViewClientDemo());
+        }
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
+
+class CheckNetwork {
+
+    private static final String TAG = CheckNetwork.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context) {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null) {
+            Log.d(TAG, "no internet connection");
+            return false;
+        } else {
+            if (info.isConnected()) {
+                Log.d(TAG, " internet connection available...");
+                return true;
+            } else {
+                Log.d(TAG, " internet connection");
+                return true;
+            }
+        }
+    }
+}
